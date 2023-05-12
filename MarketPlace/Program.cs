3d50d32d2c64c5dc8458c3bcf697405fd6f40925
Jem <116412303+minatamis@users.noreply.github.com>
2023-05-12 15:34:05 +0800

    Add project files.

---
+++ MarketPlace/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarketPlace
+{
+    class items
+    {
+        public string itemName { get; set; }
+        public string itemCategoory { get; set; }
+        public int itemPrice { get; set; }
+
+    }
+    internal class Program
+    {
+        static List<items> cartItems = new List<items>();
+
+
+        static void viewMarket()
+        {
+
+        }
+
+        static void viewwCart()
+        {
+            Console.WriteLine("Items in cart:");
+            foreach (items cartItem in cartItems)
+            {
+                Console.WriteLine("- " + cartItem);
+
+            }
+
+        }
+        static void checkOut()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. Add item to cart");
+                Console.WriteLine("2. View cart");
+                Console.WriteLine("3. Checkout");
+                Console.WriteLine("4. Exit");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        viewMarket();
+                        break;
+
+                    case 2:
+                        viewwCart();
+                        break;
+
+                    case 3:
+                        checkOut();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Goodbye!");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+
+}
