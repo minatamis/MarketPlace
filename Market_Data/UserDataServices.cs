6e7325d3e6040c8b7774435c1c9e47a0e47966c9
Eunice Stephanie Emata <116412112+ITeunice@users.noreply.github.com>
2023-06-30 17:56:33 +0800

    Save user data

--- Market_Data/UserDataServices.cs
+++ Market_Data/UserDataServices.cs
@@ -10,5 +10,18 @@ namespace Market_Data
     public class UserDataServices
     {
         public static  List<UserInfo> users = new List<UserInfo>();
+        public void SaveUserData(string userName, string userAddress, string userEmail, int userMobile)
+        {
+            UserInfo newUser = new UserInfo
+            {
+                username = userName,
+                useraddress = userAddress,
+                useremail = userEmail,
+                usermobile = userMobile
+            };
+
+            users.Add(newUser);
+            Console.WriteLine("User data saved successfully.");
+        }
     }
 }
