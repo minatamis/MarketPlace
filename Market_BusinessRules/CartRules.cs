4720d23b7691980b239a19feaa1f2a142bb8041a
Eunice Stephanie Emata <116810071+rishingzzzz@users.noreply.github.com>
2023-07-10 16:30:04 +0800

    getProduct Cart

--- Market_BusinessRules/CartRules.cs
+++ Market_BusinessRules/CartRules.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Market_Data;
+using Market_Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,19 @@ namespace Market_BusinessRules
 {
     internal class CartRules
     {
+        public static string getProduct(Cart cart)
+        {
+            string Cart = "";
 
+            Cart += $"Item Name: {cart.itemName}\n";
+            Cart += $"Price: {cart.itemPrice}\n";
+            Cart += $"Category: {cart.itemCategory} \n";
+            Cart += $"Description: {cart.itemDescription} \n";
+            Cart += $"Reason for Selling: {cart.itemRFS} \n";
+
+            return Cart;
+
+        }
 
     }
 }
--- Market_Models/Cart.cs
+++ Market_Models/Cart.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Market_Models
 {
-    internal class Cart
+    public class Cart
     {
         public string itemName { get; set; }
         public double itemPrice { get; set; }
