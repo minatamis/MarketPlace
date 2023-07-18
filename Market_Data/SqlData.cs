237a9b6eb535020f3bf01a767aa93fe5f77902db
Jem <116412303+minatamis@users.noreply.github.com>
2023-07-18 22:41:14 +0800

    creation of sql commands

---
+++ Market_Data/SqlData.cs
@@ -0,0 +1,89 @@
+﻿using Market_Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_Data
+{
+    internal class SqlData
+    {
+        static string connectionString = "Data Source = MYOUI\\SQLEXPRESS;Initial Catalog = MarketPlace;Integrated Security = True;";
+        static SqlConnection sqlConnection;
+        public SqlData()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+
+        }
+        public List<ProductsInfo> GetProduct()
+        {
+            string selectStatement = "SELECT Name, Price FROM ListedProducts";
+            SqlCommand sqlCommand = new SqlCommand(selectStatement, sqlConnection);
+            sqlConnection.Open();
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            var prodList = new List<ProductsInfo>();
+
+            while(sqlDataReader.Read())
+            {
+                prodList.Add(new ProductsInfo
+                {
+                    itemName = sqlDataReader["Name"].ToString(),
+                    itemPrice = Convert.ToDouble(sqlDataReader["Price"].ToString())
+
+                });
+            }
+            sqlConnection.Close();
+
+            return prodList;
+        }
+        public void AddProduct(ProductsInfo productsInfo)
+        {
+            string insertStatement = "INSERT INTO ListedProducts VALUES (@Name, @Price, @Category, @Description, @RFS)";
+            SqlCommand sqlCommand = new SqlCommand(insertStatement, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@Name", productsInfo.itemName);
+            sqlCommand.Parameters.AddWithValue("@Price", productsInfo.itemPrice);
+            sqlCommand.Parameters.AddWithValue("@Category", productsInfo.itemCategory);
+            sqlCommand.Parameters.AddWithValue("@Description", productsInfo.itemDescription);
+            sqlCommand.Parameters.AddWithValue("@RFS", productsInfo.itemRFS);
+
+            sqlConnection.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+        }
+        public List<ProductsInfo> GetProductsInfos()
+        {
+            string selectStatement = "SELECT * FROM ListedProducts";
+            SqlCommand sqlCommand = new SqlCommand(selectStatement, sqlConnection);
+            sqlConnection.Open();
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            var prodList = new List<ProductsInfo>();
+
+            while (sqlDataReader.Read())
+            {
+                prodList.Add(new ProductsInfo
+                {
+                    itemName = sqlDataReader["Name"].ToString(),
+                    itemPrice = Convert.ToDouble(sqlDataReader["Price"].ToString()),
+                    itemCategory = sqlDataReader["Category"].ToString(),
+                    itemDescription = sqlDataReader["Description"].ToString(),
+                    itemRFS = sqlDataReader["RFS"].ToString()
+
+                });
+            }
+            sqlConnection.Close();
+
+            return prodList;
+        }
+
+        public void UpdateProductInfo(ProductsInfo productToUpdate)
+        {
+            //to figure out
+        }
+
+    }
+
+}
